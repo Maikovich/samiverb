5e532463cbb3cd4fdfb5896baa5a28d8338e2b26
Mike Voets <mwhg.voets@gmail.com>
2015-04-09 03:55:32 +0900

    assets directory to right place

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,9 +1,11 @@
-set :application, "set your application name here"
-set :domain,      "#{application}.com"
-set :deploy_to,   "/var/www/#{domain}"
-set :app_path,    "app"
+set :application, "Samiverb"
+set :domain,      "samiverb.com"
+set :deploy_to,   "/home/maikovich/www"
 
-set :repository,  "#{domain}:/var/repos/#{application}.git"
+set :use_sudo,      false
+set :user, "maikovich"
+
+set :repository,  "git@github.com:Maikovich/samiverb.git"
 set :scm,         :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
 
@@ -15,5 +17,17 @@ role :app,        domain, :primary => true       # This may be the same as your 
 
 set  :keep_releases,  3
 
+set :shared_files,      ["app/config/parameters.yml"]
+set :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor"]
+set :use_composer, true
+set :update_vendors, true
+
+set :writable_dirs,       ["app/cache", "app/logs", "app/sessions"]
+
+logger.level = Logger::MAX_LEVEL
+
+default_run_options[:pty] = true
+
+ssh_options[:forward_agent] = true
 # Be more verbose by uncommenting the following line
 # logger.level = Logger::MAX_LEVEL
