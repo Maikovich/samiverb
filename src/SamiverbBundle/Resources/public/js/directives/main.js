d803ab74f06d628b402d40bd02912f8c89d5e164
Mike Voets <mwhg.voets@gmail.com>
2015-04-09 04:47:22 +0900

    directives updated

--- src/SamiverbBundle/Resources/public/js/directives/main.js
+++ src/SamiverbBundle/Resources/public/js/directives/main.js
@@ -6,42 +6,42 @@
       restricted: 'AE',
       templateUrl: apiRoot + '/bundles/samiverb/directives/frontpage/conjugate-form.html'
     };
-  }]);
+  }])
 
   .directive("alertSuggestions", [ 'apiRoot', function(apiRoot) {
     return {
       restricted: 'AE',
       templateUrl: apiRoot + '/bundles/samiverb/directives/alert/suggestions.html'
     };
-  }]);
+  }])
 
   .directive("alertNotvalidated", [ 'apiRoot', function(apiRoot) {
     return {
       restricted: 'AE',
       templateUrl: apiRoot + '/bundles/samiverb/directives/alert/notvalidated.html'
     };
-  }]);
+  }])
 
   .directive("alertInvalid", [ 'apiRoot', function(apiRoot) {
     return {
       restricted: 'AE',
       templateUrl: apiRoot + '/bundles/samiverb/directives/alert/invalid.html'
     };
-  }]);
+  }])
 
   .directive("verbresult", [ 'apiRoot', function(apiRoot) {
     return {
       restricted: 'AE',
       templateUrl: apiRoot + '/bundles/samiverb/directives/result/verbtable.html'
     };
-  }]);
+  }])
 
   .directive("validateModal", [ 'apiRoot', function(apiRoot) {
     return {
       restricted: 'AE',
       templateUrl: apiRoot + '/bundles/samiverb/directives/modal/validate.html'
     };
-  }]);
+  }])
 
   .directive("revalidateModal", [ 'apiRoot', function(apiRoot) {
     return {
