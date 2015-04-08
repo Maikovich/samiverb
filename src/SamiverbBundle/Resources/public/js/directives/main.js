7fa3b37271107757895fa91696a18f008ebb5e29
Mike Voets <mwhg.voets@gmail.com>
2015-04-09 04:59:48 +0900

    syntax error directives

--- src/SamiverbBundle/Resources/public/js/directives/main.js
+++ src/SamiverbBundle/Resources/public/js/directives/main.js
@@ -6,47 +6,47 @@
       restricted: 'AE',
       templateUrl: '/bundles/samiverb/directives/frontpage/conjugate-form.html'
     };
-  }])
+  })
 
   .directive("alertSuggestions", function() {
     return {
       restricted: 'AE',
       templateUrl: '/bundles/samiverb/directives/alert/suggestions.html'
     };
-  }])
+  })
 
   .directive("alertNotvalidated", function() {
     return {
       restricted: 'AE',
       templateUrl: '/bundles/samiverb/directives/alert/notvalidated.html'
     };
-  }])
+  })
 
   .directive("alertInvalid", function() {
     return {
       restricted: 'AE',
       templateUrl: '/bundles/samiverb/directives/alert/invalid.html'
     };
-  }])
+  })
 
   .directive("verbresult", function() {
     return {
       restricted: 'AE',
       templateUrl: '/bundles/samiverb/directives/result/verbtable.html'
     };
-  }])
+  })
 
   .directive("validateModal", function() {
     return {
       restricted: 'AE',
       templateUrl: '/bundles/samiverb/directives/modal/validate.html'
     };
-  }])
+  })
 
   .directive("revalidateModal", function() {
     return {
       restricted: 'AE',
       templateUrl: '/bundles/samiverb/directives/modal/revalidate.html'
     };
-  }]);
+  });
 })();
